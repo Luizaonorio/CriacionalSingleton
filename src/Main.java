44fcf80eaa35614fbaf1ace7e45d7df72fd6c70c
luizaonorio <luiza.onorio@hotmail.com>
2022-05-17 13:39:20 -0300

    Singleton

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SingleObject object1 = SingleObject.getInstance();
+        object1.showMessage();
+
+        SingleObject object2 = SingleObject.getInstance();
+        object2.showMessage();
+    }
+        }
